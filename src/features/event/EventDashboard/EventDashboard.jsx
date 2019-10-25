3db23dcc707feae894558606d82fa29bab3a653e
Oussema Belaid <ob@whitecapetech.local>
2019-10-25 15:03:59 +0100

    add routing to the app

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -3,6 +3,7 @@ import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
 import cuid from 'cuid'
+// events initializations
 const eventsFromDashboard = [
   {
     id: '1',
@@ -56,6 +57,7 @@ const eventsFromDashboard = [
 
 
 class EventDashboard extends Component {
+  //initialize state 
     state = {
         events : eventsFromDashboard,
         isOpened : false,
@@ -67,18 +69,22 @@ class EventDashboard extends Component {
     //          isOpened:!isOpened
     //      }))
     // }
-
+// function that change the state of form interface from close to open
+// the function will be passed from parent to child (eventDashboard => Event form)via the props and when submit button
+// get trigered it will pass the event to the handleCreateEvent and it will bind the event list component
    handleCreateFormOpen = () => {
      this.setState({
        isOpened:true,
        selectedEvent:null
      })
    }
+ // function that change the state of form interface from open to close
    handleFormCancel = () => {
      this.setState({
        isOpened : false
      })
    }
+   //function that create in event when submit button clicked with closing the form interface
     handleCreateEvent = (newEvent) => {
         newEvent.id = cuid();
         newEvent.hostPhotoURL ='/assets/user.png';
@@ -87,7 +93,7 @@ class EventDashboard extends Component {
             isOpened : false
         }))
    }
- 
+ //fonction that handle the select of in event and showin it in form interface when view button clicked
    handleSelectEvent = (event) => {
      this.setState({
       selectedEvent:event,
@@ -95,6 +101,7 @@ class EventDashboard extends Component {
      })
 
    }
+   // function that handle the update of in event throw the form interface
    handleUpdateEvent =(updatedEvent) => {
      this.setState(({events}) => ({
        events : events.map(event => {
@@ -109,6 +116,7 @@ class EventDashboard extends Component {
        selectedEvent : null
      }))
    }
+   // function that handle the deleting of in event
    handleDeleteEvent = (id) => {
      this.setState(({events}) => ({
        events:events.filter(e => e.id !== id)
